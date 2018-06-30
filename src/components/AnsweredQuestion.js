37be1808a69d88382343e04d629a8b7651d6a523
Duong Vi Phat <viphat@gmail.com>
2018-06-30 11:22:38 +0700

    Answered Questions

--- src/components/AnsweredQuestion.js
+++ src/components/AnsweredQuestion.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { formatQuestion } from '../utils/helpers'
+import CardHeader from './CardHeader'
+import { Link, withRouter } from 'react-router-dom'
 
 class AnsweredQuestion extends Component {
   render() {
-    const { id } = this.props
+    const { id, onDashboard, question, selectedAnswer } = this.props
     return (
-      <div>{id}</div>
+      <div className='card'>
+        { onDashboard === true
+          ?
+            <Link to={`/questions/${id}`} className='card-header-link' >
+              <CardHeader question={question}/>
+            </Link>
+          :
+            <CardHeader question={question}/>
+        }
+        <div className='card-block' style={{ padding: '10px' }}>
+          <div
+            className={ 'item ' + (selectedAnswer === 'optionOne' ? 'text-danger' : '')} >
+            { question.optionOneText }: { question.optionOneVotesCount } votes ~ { question.optionOneVotesPercent } %
+          </div>
+          <div
+            className={ 'item ' +  (selectedAnswer === 'optionTwo' ? 'text-danger' : '')}
+            >
+            { question.optionTwoText }: { question.optionTwoVotesCount } votes ~ { question.optionTwoVotesPercent }%
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-// function mapStateToProps({ }) {
+function mapStateToProps({ authedUser, questions, users }, { id }) {
+  const currentUser = users[authedUser]
+  const question = questions[id]
+  const author = users[question.author]
+  const selectedAnswer = currentUser.answers[id]
+  return {
+    question: formatQuestion(question, author, authedUser),
+    selectedAnswer,
+  }
+}
 
-// }
-
-export default connect()(AnsweredQuestion)
+export default withRouter(connect(mapStateToProps)(AnsweredQuestion))
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,19 @@
 export function formatQuestion(question, author, authedUser) {
   const { id, optionOne, optionTwo } = question
   const { avatarURL, name } = author
+  const optionOneVotesCount = optionOne.votes.length
+  const optionTwoVotesCount = optionTwo.votes.length
+  const optionOneVotesPercent = Math.round(optionOneVotesCount / (optionOneVotesCount + optionTwoVotesCount) * 100.0)
+  const optionTwoVotesPercent = 100.0 - optionOneVotesPercent
+
   return {
     id,
     optionOneText: optionOne.text,
     optionTwoText: optionTwo.text,
+    optionOneVotesCount,
+    optionTwoVotesCount,
+    optionOneVotesPercent,
+    optionTwoVotesPercent,
     avatar: avatarURL,
     authorName: name
   }
